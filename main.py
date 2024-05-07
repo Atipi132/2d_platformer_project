ee7e11c66012b3c2844747e3c763d344f141766c
atipi132 <hugo.pothieux@protonmail.com>
2024-05-07 12:45:04 +0200

    Correction de bugs + ajout de code pour mettre le jeu en pause

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import pygame_widgets.button
+import pygame_widgets.textbox
 from map import Map
 from player import Player
 from ennemy import Ennemy
+
+
 def main():
     # Screen dimensions
     WIDTH, HEIGHT = 800, 602
@@ -40,37 +44,64 @@ def main():
     
     npc = Ennemy(WIDTH // 2 - 200, HEIGHT // 2 + 100, map, player)
     npc_sprite_group.add(npc)
-
+    
+    pygame.font.init()
     pygame.init()
+    
+    quit_button = pygame_widgets.button.Button(
+        screen, 375, 291, 50, 20, 
+        text='Quit',
+        fontSize=15, margin=0,
+        inactiveColour=(255, 255, 255),
+        pressedColour=(0, 255, 15), 
+        radius=0,
+        onClick= lambda: pygame.quit()
+    )
 
     # Setup clock
     clock = pygame.time.Clock()
 
+    paused = False
     running = True
+    pause_cooldown = 0
     while running and not player.dead:
+        clock.tick(60)
+
         # Check for game quit event
-        for event in pygame.event.get():
+        key = pygame.key.get_pressed()
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 running = False
+        if pause_cooldown > 0:
+            pause_cooldown -= 1
+        elif key[pygame.K_ESCAPE]:
+            paused = not paused
+            pause_cooldown = 15
 
-        # Update player position
-        pygame.event.pump()
-        player.update()
-        npc.update()
 
         # Draw background
         for y in range(0, 600, 512):
                 for x in range(0, 800, 512):
                     screen.blit(background, (x,y))
                     
+        # Update player position
+        pygame.event.pump()
+        if not paused:
+            player.update()
+            npc.update()
+            quit_button.hide()
+        else : 
+            quit_button.draw()
+            quit_button.show()
+            pygame_widgets.update(events)
+
         # Draw screen
         player.draw(screen)
         npc_sprite_group.draw(screen)
         map.draw(screen)
         pygame.display.flip()
-
-        clock.tick(60)
-
+    
     pygame.quit()
 
 
