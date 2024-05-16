97bde6c135a368bd8c8acad222dbe6d4f53d770e
YS <testdevys@gmail.com>
2024-05-16 10:24:30 +0200

    Ajout de fichier notamment ceux relatif avec à Filed et les sprites

---
+++ groups.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.math import Vector2 as vector
+from settings import *
+
+from sprite import Sprite
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = vector()
+
+    def draw(self, target_position, timeF):
+        self.offset.x = -(target_position[0] - WIDTH/2)
+        self.offset.y = -(target_position[1] - HEIGHT / 2)
+        for sprite in self:
+            offset_position = sprite.rect.topleft + self.offset
+            self.display_surface.blit(sprite.image, offset_position)
