0634b2328e0685213f2b22d6eef10b262748f00d
YS <testdevys@gmail.com>
2024-05-16 10:54:43 +0200

    Ajout de 3 fichiers .py, level, settings et support Level gère un niveau Settings est un fichier utilitaire pour les librairies et certaines variables graphiques Support est un fichier utilitaire pour trouver l'emplacement d'autres fichiers dans le projet (trouver un dossier, trouver une image...)

---
+++ level.py
@@ -0,0 +1,32 @@
+import pygame
+from sprite import Sprite
+from player import Player
+from groups import AllSprites
+from settings import *
+
+class Level:
+    def __init__(self, tmx_map, level_frames):
+        self.display_surface = pygame.display.get_surface()
+
+        self.all_sprites = AllSprites()
+        self.collision_sprites = pygame.sprite.Group()
+
+        self.setup(tmx_map, level_frames)
+
+    def setup(self, tmx_map, level_frames):
+        for x,y, surface in tmx_map.get_layer_by_name('Terrain').tiles():
+            Sprite((x * TILE_SIZE , y * TILE_SIZE ), surface, (self.all_sprites, self.collision_sprites))
+
+        for obj in tmx_map.get_layer_by_name('Objects'):
+            if obj.name == 'Player':
+                self.player = Player(
+                    position = (obj.x, obj.y),
+                    group = self.all_sprites,
+                    collision_sprites = self.collision_sprites,
+                    frames = level_frames['player']
+                )
+
+    def run(self, timeF):
+        self.all_sprites.update(timeF)
+        self.display_surface.fill('green')
+        self.all_sprites.draw(self.player.rect.center, timeF)
