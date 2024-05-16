e55f8ffa2bd77aacd7aa55380303c7b79e952f7d
YS <testdevys@gmail.com>
2024-05-16 16:36:19 +0200

    Timer

--- Fichier Tiled/Objects.tsx
+++ Fichier Tiled/Objects.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Objects" tilewidth="15" tileheight="31" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="Objects" tilewidth="24" tileheight="32" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="15" height="31" source="../sprites/RedHoodSprite/Idle/01.png"/>
+ </tile>
+ <tile id="1">
+  <image width="24" height="32" source="../sprites/Squelette/Idle/Squelette-Idle (1).png"/>
  </tile>
 </tileset>
