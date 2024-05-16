3ac25932d536b875294a92d96c08b927834a5321
YS <testdevys@gmail.com>
2024-05-16 10:58:15 +0200

    Modification du main Actualisation du main pour supporter des nouvelles modifications : Ajout d'une class Game Ajout des imports tiled Ajout de la façon d'ouvrir un niveau fait sur Tiled

--- main.py
+++ main.py
@@ -1,109 +1,86 @@
+from settings import *
 import pygame
+from pytmx.util_pygame import load_pygame
+from level import Level
+from support import *
 import pygame_widgets.button
 import pygame_widgets.textbox
-from map import Map
-from player import Player
-from ennemy import Ennemy
 
-
-def main():
-    # Screen dimensions
-    WIDTH, HEIGHT = 800, 602
-
-    # Create screen
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-    # Set window title
-    pygame.display.set_caption("DKC Remaster")
-
-    # Set background
-    background = pygame.image.load("background/bg.png")
-
-    # Set map
-    map = Map("""
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXX
-              XoooooXXXXXXXXXXXX
-              oooooooooooooooooo
-              XXXXoooXXXXXXXXXXX
-              """).load()
-    
-    player = Player(WIDTH // 2, HEIGHT // 2, map)
-
-    npc_sprite_group = pygame.sprite.Group()
-    
-    npc = Ennemy(WIDTH // 2 - 200, HEIGHT // 2 + 100, map, player)
-    npc_sprite_group.add(npc)
-    
-    pygame.font.init()
-    pygame.init()
-    
-    quit_button = pygame_widgets.button.Button(
-        screen, 375, 291, 50, 20, 
+quit_button = pygame_widgets.button.Button(
+        screen, 375, 291, 50, 20,
         text='Quit',
         fontSize=15, margin=0,
         inactiveColour=(255, 255, 255),
-        pressedColour=(0, 255, 15), 
+        pressedColour=(0, 255, 15),
         radius=0,
         onClick= lambda: pygame.quit()
     )
 
-    # Setup clock
-    clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
 
-    paused = False
-    running = True
-    pause_cooldown = 0
-    while running and not player.dead:
-        clock.tick(60)
+        pygame.init()
 
-        # Check for game quit event
-        key = pygame.key.get_pressed()
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                running = False
-        if pause_cooldown > 0:
-            pause_cooldown -= 1
-        elif key[pygame.K_ESCAPE]:
-            paused = not paused
-            pause_cooldown = 15
+        # Initialisation de l'horloge interne
+        self.clock = pygame.time.Clock()
 
+        # Creation de l ecran de jeu
+        self.display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
-        # Draw background
-        for y in range(0, 600, 512):
-                for x in range(0, 800, 512):
-                    screen.blit(background, (x,y))
-                    
+        # Set window title
+        pygame.display.set_caption("Escape of the RedHood")
+
+        self.assets()
+
+        self.tmx_maps = {0: load_pygame("Fichier Tiled/NiveauTest2.tmx")}
+        self.current_stage = Level(self.tmx_maps[0], self.level_frames)
+
+        paused = False
+        running = False
+        pause_cooldown = 0
+
+    def run(self):
+        running = True
+        timeF = self.clock.tick()/1000
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+            self.current_stage.run(timeF)
+            pygame.display.update()
+
+    def assets(self):
+        self.level_frames = {
+            'player': import_sub_folders('sprites', 'RedHoodSprite')
+        }
+
+    def pause(self):
+        while running :
+            # Check for game quit event
+            key = pygame.key.get_pressed()
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    running = False
+            if pause_cooldown > 0:
+                pause_cooldown -= 1
+            elif key[pygame.K_ESCAPE]:
+                paused = not paused
+                pause_cooldown = 15
+
         # Update player position
         pygame.event.pump()
         if not paused:
-            player.update()
-            npc.update()
+            game.run()
             quit_button.hide()
-        else : 
+        else :
             quit_button.draw()
             quit_button.show()
             pygame_widgets.update(events)
 
-        # Draw screen
-        player.draw(screen)
-        npc_sprite_group.draw(screen)
-        map.draw(screen)
-        pygame.display.flip()
-    
     pygame.quit()
 
-
 if __name__ == "__main__":
-    main()
+    game = Game()
+    game.run()
