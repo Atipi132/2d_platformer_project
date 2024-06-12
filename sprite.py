f73bbbf2415694bb7b5e779bd324d69561a16b17
YS <testdevys@gmail.com>
2024-06-12 17:03:58 +0200

    Suppression classe inutile

--- sprite.py
+++ sprite.py
@@ -9,15 +9,3 @@ class Sprite(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = position)
         self.old_rect = self.rect.copy()
 
-class AnimatedSprite(Sprite):
-    def __init__(self, position, frames, groups, animation_speed = ANIMATION_SPEED):
-        self.frames, self.frame_index = frames, 0
-        super().__init__(position, self.frames[self.frame_index], groups)
-        self.animation_speed = animation_speed
-
-    def animate(self, timeF):
-        self.frame_index += self.animation_speed * timeF
-        self.image = self.frames[int(self.frame_index % len(self.frames))]
-
-    def update(self, time):
-        self.animate(time)
