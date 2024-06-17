6f3c7898a8c1cce8c64599d9ec9e82056a4e0da3
YS <testdevys@gmail.com>
2024-06-17 11:59:32 +0200

    Indentation

--- sprite.py
+++ sprite.py
@@ -1,11 +1,12 @@
 import pygame
 from settings import *
 
+
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, position, surface, group):
         super().__init__(group)
 
         self.image = surface
-        self.rect = self.image.get_rect(topleft = position)
+        self.rect = self.image.get_rect(topleft=position)
         self.old_rect = self.rect.copy()
 
--- support.py
+++ support.py
@@ -2,17 +2,20 @@ from os import walk
 from os.path import join
 import pygame
 
-def import_image(*path, alpha = True, format = 'png'):
+
+def import_image(*path, alpha=True, format='png'):
 	full_path = join(*path) + f'.{format}'
 	return pygame.image.load(full_path).convert_alpha() if alpha else pygame.image.load(full_path).convert()
+
 
 def import_folder(*path):
 	frames = []
 	for folder_path, subfolders, image_names in walk(join(*path)):
-		for image_name in sorted(image_names, key = lambda name: int(name.split('.')[0])):
+		for image_name in sorted(image_names, key=lambda name: int(name.split('.')[0])):
 			full_path = join(folder_path, image_name)
 			frames.append(pygame.image.load(full_path).convert_alpha())
 	return frames
+
 
 def import_folder_dict(*path):
 	frame_dict = {}
@@ -22,6 +25,7 @@ def import_folder_dict(*path):
 			surface = pygame.image.load(full_path).convert_alpha()
 			frame_dict[image_name.split('.')[0]] = surface
 	return frame_dict
+
 
 def import_sub_folders(*path):
 	frame_dict = {}
