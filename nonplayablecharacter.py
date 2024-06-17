c1c711cc167c8b731442f588f39ebb1bb6e4c45a
YS <testdevys@gmail.com>
2024-06-17 12:05:32 +0200

    Indentation

--- nonplayablecharacter.py
+++ nonplayablecharacter.py
@@ -1,6 +1,7 @@
 from settings import *
 from player import Player
 
+
 class NonPlayableCharacter(Player):
     def __init__(self, position, group, collision_sprites, frames):
         super().__init__(position, group, collision_sprites, frames)
